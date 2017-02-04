432cbf132f6b834a022d75af59fc1ab31ecaf5e7
Jeyaraj <vignesh.jeyaraj@nike.com>
2017-02-04 11:26:28 -0800

    Base code with react and webpack dev server.

---
+++ index.js
@@ -0,0 +1,15 @@
+const {render} = ReactDOM;
+
+const style = {
+  backgroundColor: "orange",
+  color: "white",
+  fontFamily: "verdana"
+}
+
+render(
+  <h1 id='title'
+      className='header'
+      style={style}>Hello World
+  </h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname + "/dist/assets",
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./dist",
+        port: 3000
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2015", "react", "stage-0"]
+                }
+            }
+        ]
+    }
+}
