ac6624a6f38ea56d09919c35a3ab56e975d3a481
Jeyaraj <vignesh.jeyaraj@nike.com>
2017-02-20 11:56:45 -0800

    Added the props for the card and included the card List.

--- app/scripts/components/Card.js
+++ app/scripts/components/Card.js
@@ -8,10 +8,21 @@ export class Card extends Component {
                     <img src='images/mixed_charcoal.jpg' />
                 </div>
                 <div className='card-desc'>
-                    <h2>Our Fields</h2>
-                    <p>Fields are located in southern parts of india</p>
+                    <h2>{this.props.cardDetails.heading}</h2>
+                    <p>{this.props.cardDetails.description}</p>
                 </div>
             </div>
         )
     }
+}
+
+Card.defaultProps = {
+    cardDetails: {
+        heading: "Our Fields",
+        description: "Fields are located in southern parts of india"
+    }
+}
+
+Card.propTypes = {
+    cardDetails: PropTypes.object
 }
